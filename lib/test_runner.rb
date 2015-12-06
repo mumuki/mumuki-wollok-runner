7debb3dd061ab404f70730018c25586c58e9552b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-06 14:27:01 -0300

    Test runner takes a filename, not a file

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -2,7 +2,7 @@ class TestRunner < Mumukit::FileTestRunner
   include Mumukit::WithIsolatedEnvironment
 
   def run_test_command(filename)
-    "#{wollok_command} #{file.path} 2>&1"
+    "#{wollok_command} #{filename} 2>&1"
   end
 
   def post_process_file(file, result, status)
