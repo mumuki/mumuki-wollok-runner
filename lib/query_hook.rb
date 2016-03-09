cd3f43e9826302639a343c3eeafa5e8321a022bf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-03-09 15:04:50 -0300

    Removing warnings from output and adding tests

--- lib/query_hook.rb
+++ lib/query_hook.rb
@@ -6,14 +6,33 @@ class QueryHook < Mumukit::Templates::FileHook
   end
 
   def compile_file_content(r)
-    "#{r.extra}\n#{r.content}\nprogram mumuki {  #{build_state(r.cookie)}  console.println('=> ' + (#{r.query}).toString()) }"
+<<WLK
+#{r.extra}
+#{r.content}
+program mumuki {
+  #{build_state(r.cookie)}
+  console.println('=> ' + (#{r.query}).toString())
+}
+WLK
   end
 
   def build_state(cookie)
-    cookie.map { |statement| "try { #{statement} } catch(Exception e) {  }" }.join("\n")
+    (cookie||[]).map { |statement| "try { #{statement} } catch e : Exception {  }" }.join("\n")
   end
 
   def command_line(filename)
     "#{wollok_command} #{filename} 2>&1"
+  end
+
+  def post_process_file(file, result, status)
+    [remove_warnings(result), status]
+  end
+
+  def remove_warnings(result)
+    result.
+        split("\n").
+        reject { |it| it.start_with?('Warning: ') || it.include?('WARNING Unused variable') }.
+        map { |it| "#{it}\n" }.
+        join
   end
 end
